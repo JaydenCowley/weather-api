97057b279b1b42ccfc9933b72a58e7601cca0864
Jayden Cowley <jcowley16@gmail.com>
2022-04-30 13:09:41 -0600

    Adding api functionality and getting adding html elements for the weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,51 @@
 //API key: dd5b88dda79c76158757fafce663aa41
 //weather retrieval: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=dd5b88dda79c76158757fafce663aa41
 //lat lon retrieval: http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=5&appid=dd5b88dda79c76158757fafce663aa41
+//defined variables
+const apiKey = "dd5b88dda79c76158757fafce663aa41";
+// submit button function
+document.getElementById("city-submit").addEventListener("click", function () {
+
+
+    // document.getElementById("city-list").appendChild("li", cityName).setAttribute("class","city-list-item");
+    //localStorage.setItem(cityName, "City Name");
+    getWeatherApi();
+    //getWeatherApi();
+
+
+});
+
+
+//getLatLonApi()
+async function getWeatherApi() {
+    var cityName = document.getElementById("city-name").value;
+    var countryName = document.getElementById("country-name").value;
+    var stateName = document.getElementById("state-name").value;
+    // Requesting the lat and lon of the entered city
+    var requestLatLonUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateName + "," + countryName + "&limit=5&appid=" + apiKey
+    await fetch(requestLatLonUrl)
+        .then(function (response) {
+            console.log(response)
+
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log("lat: " + data[0].lat + ", " + "lon: " + data[0].lon);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            //Requesting the weather data using the lat an lon provided by the last request
+            var requestWeatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
+            fetch(requestWeatherUrl)
+                .then(function (response) {
+                    console.log(response)
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data)
+                })
+                //Adding collected data to the html
+                .then(function(data) {
+                })
+        });
+};
