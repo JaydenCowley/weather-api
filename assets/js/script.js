56830fca5bab24e71b3bac4cbf8b1837890aab0b
Jayden Cowley <jcowley16@gmail.com>
2022-05-07 17:17:03 -0600

    Adding icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,24 +15,38 @@
 //lat lon retrieval: http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=5&appid=dd5b88dda79c76158757fafce663aa41
 //defined variables
 const apiKey = "dd5b88dda79c76158757fafce663aa41";
+
 // submit button function
 document.getElementById("city-submit").addEventListener("click", function () {
+    var cityName = document.getElementById("city-name").value;
+    var countryName = document.getElementById("country-name").value;
+    var stateName = document.getElementById("state-name").value;
+    //document.createElement("li").setAttribute("class", "list-item")
+    //document.getElementById("list-item") = cityName + ", " + stateName + ", " + countryName
 
-
-    // document.getElementById("city-list").appendChild("li", cityName).setAttribute("class","city-list-item");
+    //document.getElementById("city-list").appendChild("li", cityName).setAttribute("class","city-list-item");
     //localStorage.setItem(cityName, "City Name");
     getWeatherApi();
-    //getWeatherApi();
-
 
 });
 var DateTime = luxon.DateTime;
-var today = DateTime.local;
+//console.log(DateTime);
+var today = DateTime.local();
 console.log(DateTime.local().c.day);
-var CurrentDayOfMonth = DateTime.local().c.day;
-var CurrentMonth = DateTime.local().c.month;
-var CurrentYear = DateTime.local().c.year;
-console.log(CurrentMonth + "/" + CurrentDayOfMonth + "/" + CurrentYear)
+console.log(today)
+console.log(DateTime.fromMillis(1651936793961));
+var getDate = function (millis) {
+    millis = millis * 1000
+    //console.log(millis)
+    //console.log(DateTime.fromMillis(millis).c.month + "/" + DateTime.fromMillis(millis).c.day + "/" + DateTime.fromMillis(millis).c.year)
+    return DateTime.fromMillis(millis).c.month + "/" + DateTime.fromMillis(millis).c.day + "/" + DateTime.fromMillis(millis).c.year
+}
+var getIcon = function (iconId) {
+    var img = "http://openweathermap.org/img/wn/" + iconId + "@2x.png"
+    return img
+}
+
+
 //Get all the information we need from the Api's()
 async function getWeatherApi() {
     var cityName = document.getElementById("city-name").value;
@@ -42,8 +56,6 @@ async function getWeatherApi() {
     var requestLatLonUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateName + "," + countryName + "&limit=5&appid=" + apiKey
     await fetch(requestLatLonUrl)
         .then(function (response) {
-            console.log(response)
-
             return response.json();
         })
         .then(function (data) {
@@ -57,38 +69,40 @@ async function getWeatherApi() {
             var requestWeatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey
             fetch(requestWeatherUrl)
                 .then(function (response) {
-                    console.log(response)
+
                     return response.json();
                 })
                 .then(function (data) {
-                    //console.log(DateTime.toString(data.current.dt));
-                    //Adding collected data to the html
-                    //Current
-                    //getDate(data.current.dt)
-                    document.getElementById("current-date").textContent = (CurrentMonth + "/" + CurrentDayOfMonth + "/" + CurrentYear);
+
+                    console.log(data)
+                    document.getElementById("current-date").textContent = getDate(data.current.dt)
+                    document.getElementById("current-icon").src = getIcon(data.current.weather[0].icon)
                     document.getElementById("current-temp").textContent = ("Temp: " + data.current.temp + "ºF");
                     document.getElementById("current-wind").textContent = ("Wind Speed: " + data.current.wind_speed + "mph");
                     document.getElementById("current-humidity").textContent = ("Humidity: " + data.current.humidity + "%");
                     document.getElementById("current-uv").textContent = ("UV index: " + data.current.uvi);
-                    if (data.current.uvi > 0) {
+                    if (data.current.uvi < 0) {
                         document.getElementById("current-uv").style.backgroundColor = "green";
                     }
-                    else if (data.current.uvi > 3) {
+                    else if (data.current.uvi < 3) {
+                        document.getElementById("current-uv").style.backgroundColor = "green";
+                    }
+                    else if (data.current.uvi < 5) {
                         document.getElementById("current-uv").style.backgroundColor = "yellow";
                     }
-                    else if (data.current.uvi > 5) {
+                    else if (data.current.uvi < 8) {
                         document.getElementById("current-uv").style.backgroundColor = "orange";
                     }
-                    else if (data.current.uvi > 8) {
-                        document.getElementById("current-uv").style.backgroundColor = "red";
-                    };
+                    else (document.getElementById("current-uv").style.backgroundColor = "red");
+                    console.log(data.daily[1].weather[0].icon)
 
 
                     for (let i = 1; i < 6; i++) {
-                        document.getElementById("day-" + i + "-date").textContent = (CurrentMonth + "/" + CurrentDayOfMonth + i + "/" + CurrentYear);
-                        document.getElementById("day-" + i + "-temp").textContent = ("Temp: " + data.daily[i - 1].temp.day + "ºF");
-                        document.getElementById("day-" + i + "-wind").textContent = ("Wind Speed: " + data.daily[i - 1].wind_speed + "mph");
-                        document.getElementById("day-" + i + "-humidity").textContent = ("Humidity: " + data.daily[i - 1].humidity + "%");
+                        document.getElementById("day-" + i + "-date").textContent = getDate(data.daily[i].dt)
+                        document.getElementById("icon-" + i).src = getIcon(data.daily[i].weather[0].icon)
+                        document.getElementById("day-" + i + "-temp").textContent = "Temp: " + data.daily[i - 1].temp.day + "ºF";
+                        document.getElementById("day-" + i + "-wind").textContent = "Wind Speed: " + data.daily[i - 1].wind_speed + "mph";
+                        document.getElementById("day-" + i + "-humidity").textContent = "Humidity: " + data.daily[i - 1].humidity + "%";
                     }
                 })
         });
